48c966598ad9eb75d5cb626a48a6da20ec37c559
sarika <Sarikabindu89@gmail.com>
2024-02-27 10:50:53 +0000

    added median value calculation function

--- cypress/e2e/Heartrate_Measurement.cy.js
+++ cypress/e2e/Heartrate_Measurement.cy.js
@@ -13,6 +13,7 @@ class Heartrate_Measurement{
               date: day,
               min: Infinity,
               max: -Infinity,
+              medianbeats:[],
               latestDataTimestamp: null
           };
       }
@@ -20,10 +21,15 @@ class Heartrate_Measurement{
         const bpm = reading.beatsPerMinute;
         resultset[day].min = Math.min(resultset[day].min, bpm);
         resultset[day].max = Math.max(resultset[day].max, bpm);
+        resultset[day].medianbeats.push(bpm);
         if (!resultset[day].latestDataTimestamp || new Date(reading.timestamps.startTime) > new Date(resultset[day].latestDataTimestamp)) {
           resultset[day].latestDataTimestamp = reading.timestamps.startTime;
         }
     });
+    //calculate median beats with in a date range 
+    for (const day in resultset) {
+      resultset[day].medianbeats = medianbeats(resultset[day].medianbeats);
+      }
     //storing the result value
     const resultsArray = Object.values(resultset);
     //creating output.json file with result
@@ -38,4 +44,18 @@ class Heartrate_Measurement{
 
   }
 }
+//Function to caluculate the median beats and returning the value
+        function medianbeats(values) 
+          {
+
+            const sorted = values.slice().sort((a, b) => a - b);
+            cy.log(sorted)
+            const middlevalue = Math.floor(sorted.length / 2);
+        
+            if (sorted.length % 2 === 0) {
+                return (sorted[middlevalue - 1] + sorted[middlevalue]) / 2;
+            }
+        
+            return sorted[middlevalue];
+        }
 module.exports={Heartrate_Measurement}
