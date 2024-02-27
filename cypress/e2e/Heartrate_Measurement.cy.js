c3f092b21f65962805e4ff274ee9d39ab2c4e81c
sarika <Sarikabindu89@gmail.com>
2024-02-27 00:39:37 +0000

    added comments

--- cypress/e2e/Heartrate_Measurement.cy.js
+++ cypress/e2e/Heartrate_Measurement.cy.js
@@ -2,6 +2,7 @@
 class Heartrate_Measurement{
   fetchheartratedata()
    {
+    //read the input data
     cy.fixture('heartrate.json').then(result =>{
       const resultset = {};
       result.forEach(reading =>{
@@ -15,30 +16,26 @@ class Heartrate_Measurement{
               latestDataTimestamp: null
           };
       }
+      //calculate min bpm and max bpm,latest data timestamp
         const bpm = reading.beatsPerMinute;
         resultset[day].min = Math.min(resultset[day].min, bpm);
         resultset[day].max = Math.max(resultset[day].max, bpm);
         if (!resultset[day].latestDataTimestamp || new Date(reading.timestamps.startTime) > new Date(resultset[day].latestDataTimestamp)) {
           resultset[day].latestDataTimestamp = reading.timestamps.startTime;
-      }
-  
-});
-const resultsArray = Object.values(resultset);
-
-  cy.writeFile('output.json', JSON.stringify(resultsArray, null, 2), err => {
-    if (err) {
-        console.error('Error writing to output.json:', err);
-    } else {
+        }
+    });
+    //storing the result value
+    const resultsArray = Object.values(resultset);
+    //creating output.json file with result
+      cy.writeFile('output.json', JSON.stringify(resultsArray, null, 2), err => {
+        if (err) {
+          console.error('Error writing to output.json:', err);
+        } else {
         console.log('Output saved to output.json');
-    }
-});
+          }
+      });
+    })
 
-
-})
-
-}
-
-  
-   
+  }
 }
 module.exports={Heartrate_Measurement}
